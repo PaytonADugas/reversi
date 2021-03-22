0dfe8c11fae4c14d86a06edf628bbd2e4bb174ec
Payton Dugas <payton.dugas@gmail.com>
2021-03-22 14:18:57 -0700

    Lobby stuff

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+/* functions for general use*/
+
+function getURLParameters(whichParam){
+  var pageURL = window.location.search.substring(1);
+  var pageURLVariables = pageURL.split('&');
+  for(let i = 0; i < pageURLVariables.length; i++){
+    var parameterName = pageURLVariables[i].split('=');
+    if(parameterName[0] == whichParam){
+      return parameterName[1];
+    }
+  }
+}
+
+
+var username = getURLParameters('username');
+if(username == null){
+  username = 'Anonymous'+Math.random();
+}
+
+$('#messages').append('<h3>'+username+'<h3>');
